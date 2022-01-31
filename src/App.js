f70072e05fa68dc038674baf842100a44bc7748d
PiyushPI07 <ingalepiyush459@gmail.com>
2022-01-31 13:05:27 +0530

    created gallary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import Gallery from "./components/Gallery";
+import Marketplace from "./components/Marketplace";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import SearchResults from "./components/SearchResults"
+import Account from "./components/Account"
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/callback" element={<Navigate replace to="/" />} />        
+          <Route exact path="/marketplace" element={<Marketplace/>}/>
+          <Route exact path="/gallery" element={<Gallery/>}/>
+          <Route exact path="/search" element={<SearchResults />} />
+          <Route exact path="/account" element={<Account />} />
+        </Routes>
+      </Router>
   );
 }
 
